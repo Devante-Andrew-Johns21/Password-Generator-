3c3d8caeed18ab22c498818d3138ab0035acd534
Devante Johns <johnsdevante74@gmail.com>
2021-01-18 17:31:21 -0500

    I creted a function that would generate a random password; you control the lenght of your password by the slider element. Ive included all numbers and symbols within the possibilities for the password

---
+++ script.js
@@ -0,0 +1,17 @@
+function generate(){
+
+    ///set length of password
+    let complexity = document.getElementById("slider").value;
+/// possible password values 
+    let values = "ABCEDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%67*()_+";
+
+    let password = "";
+
+    ///create forloop for password characters
+    for(var i = 0 i <= complexity; i++){
+
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+///add password to display 
+document.getElementById("display").value = password;
+}
