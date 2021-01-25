beb79bd47737a2114e4fa3834ec6fd15c2d33010
Devante Johns <johnsdevante74@gmail.com>
2021-01-25 16:23:57 -0500

    updated ReadMe

--- script.js
+++ script.js
@@ -1,44 +1,14 @@
-function generate(){
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-    ///set length of password
-    let complexity = document.getElementById("slider").value;
-/// possible password values 
-    let values = "ABCEDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%67*()_+";
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-    let password = "";
-
-    ///create forloop for password characters
-    for(var i = 0; i <= complexity; i++){
-
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-    }
-///add password to display 
-document.getElementById("display").value = password;
-
-///add password to previously generated passwords section 
-document.getElementById("lastNums").innerHTML += password + "<br />";
-
-
+  passwordText.value = password;
 
 }
 
-//set default length display of 25
-document.getElementById("length").innerHTML = "length: 25"
-
-//function to set length by slider 
-document.getElementById("slider").oninput = function(){
-
-if(document.getElementById("slider").value > 0){
- document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
-}
-else{
-    document.getElementById("length").innerHTML = "Length: 1";
-
- }}
-//function to copy PW to clipboard          
-function copyPassword(){
-    document.getElementById("display").select();
-    document.execCommand("Copy");
-
-    alert("Password copied to clipboard")
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
