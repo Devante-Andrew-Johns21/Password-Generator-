79550652f7bebd5c95c8f8520838ef0068ff223e
Devante Johns <johnsdevante74@gmail.com>
2021-02-06 01:24:21 -0500

    I added a row within a row for formatting purposes and i set some padding- bottom and top

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
- Write password to the #password input
-function writePassword() {//
+ //Write password to the #password input
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
